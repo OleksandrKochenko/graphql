4b5d213081df7de79b85763f27c3342ef5b06ae8
Alex Kochenko <kochenko.fop@gmail.com>
2025-05-13 14:43:58 +0200

    ts, server, gql schemas

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs, resolvers } from "./schema.js";
+// server setup
+const server = new ApolloServer({
+    // typeDefs -- definitions of types of data
+    typeDefs,
+    // resolvers -- functions that resolve the data
+    resolvers,
+});
+console.log("Starting server...");
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
---
+++ dist/schema.js
@@ -0,0 +1,36 @@
+import db from "./_db.js";
+export const typeDefs = `#graphql
+# schema for a game graphql api -- coresponds to the data at games table in the database
+type Game {
+    id: ID! # the exclamation mark means that the field is required
+    title: String!
+    platform: [String!]!
+}
+# schema for a review graphql api
+type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+}
+# schema for an author graphql api
+type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+}
+# schema for a query graphql api -- defines what arrays of data we can query
+# and what types of data they return
+type Query {
+    reviews: [Review]
+    games: [Game]
+    authors: [Author]
+}
+`;
+// graphQL types: int, float, string, boolean, ID
+export const resolvers = {
+    Query: {
+        reviews: () => db.reviews,
+        games: () => db.games,
+        authors: () => db.authors,
+    },
+};
